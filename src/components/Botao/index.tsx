29910bca703ca151eab2569b842a208b9099f5c1
Rivaldo Jr <rivaldojr@gmail.com>
2022-11-09 15:19:40 -0300

    02

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Botao.module.scss'
+
+class Botao extends React.Component<any, string> {
+    render(){
+        return (
+            <button className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
